4262f5fb585cb7b0cd1e2b75d9394905678152f7
Simon-157 <boatengsimonjnr157@gmail.com>
2024-06-05 21:19:56 +0000

    Enhance  background workers 💡

--- websocket/queues/videoQueue.ts
+++ websocket/queues/videoQueue.ts
@@ -1,17 +1,22 @@
-const rabbitmq = require('../config/rabbit');
-import { VideoData } from '../utils/types';
+import { logger } from "../config/logger";
+import * as rabbitmq from '../config/rabbit'; 
 
-const AUDIO_QUEUE_NAME = 'audio_queue';
+const VIDEO_QUEUE_NAME = 'video_queue';
 
-async function init() {
+export const init = async () => {
     await rabbitmq.connect();
     const channel = rabbitmq.getChannel();
-    await channel.assertQueue(AUDIO_QUEUE_NAME);
+    await channel?.assertQueue(VIDEO_QUEUE_NAME, { durable: true });
+    
 }
 
-async function publishAudioMessage(data:VideoData) {
-    const channel = rabbitmq.getChannel();
-    channel.sendToQueue(AUDIO_QUEUE_NAME, Buffer.from(JSON.stringify(data)));
+export const publishVideoMessage = async (userId: string, sessionId: string) => {
+    try {
+        const channel = rabbitmq.getChannel();
+        const message = JSON.stringify({ userId, sessionId });
+        console.log('Publishing video message:', message);
+        await channel?.sendToQueue(VIDEO_QUEUE_NAME, Buffer.from(message));
+    } catch (error) {
+        logger.error('Error publishing video message:', error);
+    }
 }
-
-module.exports = { init, publishAudioMessage };
